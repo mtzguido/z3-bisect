d43a4a5ca97916237268702a2e53d13deede8ddc
Guido Martínez <mtzguido@gmail.com>
2023-06-21 10:37:44 -0700

    disable optimizations when building z3

--- build-and-test.sh
+++ build-and-test.sh
@@ -13,6 +13,11 @@ cd $(dirname $0)
 pushd z3
 git clean -dffx &>/dev/null
 ./configure &>>BUILDLOG
+
+# Try to disable optimizations for a faster build. It's likely
+# that building Z3 takes longer than actually running the test.
+sed -i 's/-O3/-O0/' build/config.mk || true
+
 if ! make -C build -j$(nproc) &>>BUILDLOG; then
 	exit 125
 fi
