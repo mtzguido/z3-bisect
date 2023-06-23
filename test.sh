ad892aa5914adb80089e27ddf382c43fd591125a
Guido Martínez <mtzguido@gmail.com>
2023-06-23 09:09:23 -0700

    bsiecting for sigsegv

--- test.sh
+++ test.sh
@@ -15,6 +15,7 @@ cd $(dirname $0)
 N=1 # Number of different seeds
 
 oom=0
+segfault=0
 unknown=0
 unsat=0
 sat=0
@@ -23,12 +24,13 @@ seed=0
 for seed in $(seq 0 $((N-1))); do
 	o=$(ramon ./z3/build/z3 smt.random_seed=$seed "${FILE}" 2>errout | tee output | tail -n1)
 	if grep SIGKILL errout; then oom=$((oom+1));
+	elif grep SIGSEGV errout; then segfault=$((segfault+1));
 	elif [ "$o" == "unknown" ]; then unknown=$((unknown+1));
 	elif [ "$o" == "unsat" ]; then unsat=$((unsat+1));
 	elif [ "$o" == "sat" ]; then sat=$((sat+1)); fi
 done
 
-if [ "$unknown" == "$N" ]; then echo BAD; exit 1; fi # bad
+if [ "$segfault" == "$N" ]; then echo BAD; exit 1; fi # bad
 if [ "$unsat" == "$N" ]; then echo GOOD; exit 0; fi # good
 
 echo INCONCLUSIVE $unknown $unsat
